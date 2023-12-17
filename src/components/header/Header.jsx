6f460ad87b00ec570063cc098dd9a36cf29fa44f
SarveshKulkarni1111 <109644906+SarveshKulkarni1111@users.noreply.github.com>
2023-12-17 22:08:17 +0530

    Added about component and responsivness

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,16 +8,13 @@ const Header = () => {
     <div>
     <Navbar />
     <div className='header-container'>
-    <div className='row'>
-    <div className='col'>
-    <h1 >Hello, I'm Sarvesh Kulkarni.</h1>
-    <h1>I'm a full-stack web developer.</h1>
-    </div>
-    <div className='col'>
-    <img src={profile} alt="profile img" className="rounded-image" />
-    </div>
-    </div>
-    </div>
+    <div className='content-container'>
+        <h1>Hello, I'm Sarvesh Kulkarni.</h1>
+        <h1>I'm a full-stack web developer.</h1>
+        </div>
+        <img src={profile} alt="profile img" className="rounded-image" />
+</div>
+
     </div>
   )
 }
