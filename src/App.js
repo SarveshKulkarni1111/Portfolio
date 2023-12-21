6497f3e7ba7a1a95f65048fdbb35285f2d86f3d7
SarveshKulkarni1111 <109644906+SarveshKulkarni1111@users.noreply.github.com>
2023-12-22 00:04:12 +0530

    Corrected file structure

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 import Projects from './components/projects/Projects';
-import Awards from './awards/awards';
+import Awards from './components/awards/awards';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { SpeedInsights } from '@vercel/speed-insights/react';
 
